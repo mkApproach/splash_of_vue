66b5405962749d994c979b057cd4bfa8c11865fb
mkApproach <kinai-ap@cameo.plala.or.jp>
2022-03-23 17:46:07 +0900

    first commit

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -15,19 +15,19 @@ const mutations = {
 
 const actions = {
   async register (context, data) {
-    const response = await axios.post('/api/register', data)
+    const response = await axios.post('./api/register', data)
     context.commit('setUser', response.data)
   },
   async login (context, data) {
-    const response = await axios.post('/api/login', data)
+    const response = await axios.post('./api/login', data)
     context.commit('setUser', response.data)
   },
   async logout (context) {
-    const response = await axios.post('/api/logout')
+    const response = await axios.post('./api/logout')
     context.commit('setUser', null)
   },
   async currentUser (context) {
-    const response = await axios.get('/api/user')
+    const response = await axios.get('./api/user')
     const user = response.data || null
     context.commit('setUser', user)
   }
